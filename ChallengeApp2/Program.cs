aad141ec67a291929a3512042b5dc0683154ecb8
Kamil Motulewicz <kamil.motulewicz@gmail.com>
2023-06-22 21:00:56 +0200

    lekcja

--- ChallengeApp2/Program.cs
+++ ChallengeApp2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+List<string> daysOfWeek = new List<string>();
+
+daysOfWeek.Add("poniedziałek");
+daysOfWeek.Add("wtorek");
+daysOfWeek.Add("środa");
+daysOfWeek.Add("czwartek");
+daysOfWeek.Add("piątek");
+daysOfWeek.Add("sobota");
+daysOfWeek.Add("niedziela");
+
+foreach (var day in daysOfWeek) 
+Console.WriteLine(day);
+
+for (int a = 0; a < daysOfWeek.Count; a++) 
+{
+    Console.WriteLine(daysOfWeek[a]);
+}
