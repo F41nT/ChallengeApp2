b9b73112d6665f6cf8a805aa4847c56b14fc3c69
Kamil Motulewicz <kamil.motulewicz@gmail.com>
2023-06-23 17:23:33 +0200

    wersja 2

--- ChallengeApp2/Program.cs
+++ ChallengeApp2/Program.cs
@@ -1,67 +1,58 @@
 ﻿using System.Data;
 
-int number = 99999;
+ulong number = ulong.MaxValue;
 string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+char[] digit = numberInString.ToArray();
+int[] counters = { 0,0,0,0,0,0,0,0,0,0 };
 
-foreach (var letter in letters)
+foreach (var letter in digit)
     if(letter == '0')
     {
-        counter0++;
+        counters[0]++;
     }    
     else if (letter == '1')
     {
-        counter1++;
+        counters[1]++;
     }
     else if (letter == '2')
     {
-        counter2++;
+        counters[2]++;
     }
     else if (letter == '3')
     {
-        counter3++;
+        counters[3]++;
     }
     else if (letter == '4')
     {
-        counter4++;
+        counters[4]++;
     }
     else if (letter == '5')
     {
-        counter5++;
+        counters[5]++;
     }
     else if (letter == '6')
     {
-        counter6++;
+        counters[6]++;
     }
     else if (letter == '7')
     {
-        counter7++;
+        counters[7]++;
     }
     else if (letter == '8')
     {
-        counter8++;
+        counters[8]++;
     }
     else if (letter == '9')
     {
-        counter9++;
+        counters[9]++;
     }
-Console.WriteLine("0 ==> " + counter0);
-Console.WriteLine("1 ==> " + counter1);
-Console.WriteLine("2 ==> " + counter2);
-Console.WriteLine("3 ==> " + counter3);
-Console.WriteLine("4 ==> " + counter4);
-Console.WriteLine("5 ==> " + counter5);
-Console.WriteLine("6 ==> " + counter6);
-Console.WriteLine("7 ==> " + counter7);
-Console.WriteLine("8 ==> " + counter8);
-Console.WriteLine("9 ==> " + counter9);
+Console.WriteLine("0 ==> " + counters[0]);
+Console.WriteLine("1 ==> " + counters[1]);
+Console.WriteLine("2 ==> " + counters[2]);
+Console.WriteLine("3 ==> " + counters[3]);
+Console.WriteLine("4 ==> " + counters[4]);
+Console.WriteLine("5 ==> " + counters[5]);
+Console.WriteLine("6 ==> " + counters[6]);
+Console.WriteLine("7 ==> " + counters[7]);
+Console.WriteLine("8 ==> " + counters[8]);
+Console.WriteLine("9 ==> " + counters[9]);
