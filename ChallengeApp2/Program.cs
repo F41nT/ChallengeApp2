ea71f52a6d0c3063299987d07346fe84c8320882
Kamil Motulewicz <kamil.motulewicz@gmail.com>
2023-06-23 17:23:54 +0200

    w

--- ChallengeApp2/Program.cs
+++ ChallengeApp2/Program.cs
@@ -2,50 +2,52 @@
 
 ulong number = ulong.MaxValue;
 string numberInString = number.ToString();
-char[] digit = numberInString.ToArray();
-int[] counters = { 0,0,0,0,0,0,0,0,0,0 };
+char[] letter = numberInString.ToArray();
+int[] counters = new int[10];
 
-foreach (var letter in digit)
-    if(letter == '0')
+foreach (var digit in letter)
+
+    if(digit == '0')
     {
         counters[0]++;
     }    
-    else if (letter == '1')
+    else if (digit == '1')
     {
         counters[1]++;
     }
-    else if (letter == '2')
+    else if (digit == '2')
     {
         counters[2]++;
     }
-    else if (letter == '3')
+    else if (digit == '3')
     {
         counters[3]++;
     }
-    else if (letter == '4')
+    else if (digit == '4')
     {
         counters[4]++;
     }
-    else if (letter == '5')
+    else if (digit == '5')
     {
         counters[5]++;
     }
-    else if (letter == '6')
+    else if (digit == '6')
     {
         counters[6]++;
     }
-    else if (letter == '7')
+    else if (digit == '7')
     {
         counters[7]++;
     }
-    else if (letter == '8')
+    else if (digit == '8')
     {
         counters[8]++;
     }
-    else if (letter == '9')
+    else if (digit == '9')
     {
         counters[9]++;
     }
+
 Console.WriteLine("0 ==> " + counters[0]);
 Console.WriteLine("1 ==> " + counters[1]);
 Console.WriteLine("2 ==> " + counters[2]);
@@ -54,5 +56,4 @@ Console.WriteLine("4 ==> " + counters[4]);
 Console.WriteLine("5 ==> " + counters[5]);
 Console.WriteLine("6 ==> " + counters[6]);
 Console.WriteLine("7 ==> " + counters[7]);
-Console.WriteLine("8 ==> " + counters[8]);
 Console.WriteLine("9 ==> " + counters[9]);
